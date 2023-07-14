7939b91014aeb2d3c1457b2cbbbcbe3ec289d5e3
Daniel Codreanu <danielcdr2006@gmail.com>
2023-07-14 19:33:48 +0300

    Add project files.

---
+++ Tema 5/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //1.Scrieti un program care va afisa pozitia unui substring intr-un string, ambele siruri de caractere fiind citite de la tastatura
+
+        //Console.WriteLine("Introduceti textul:");
+        //string sir = onsole.ReadLine();
+
+        //Console.WriteLine("Introduceti subsirul:");
+        //string substring = Console.ReadLine();
+
+        //int pozitia = sir.IndexOf(substring);
+
+        //Console.WriteLine($"Substringul este la pozitia {pozitia} in sirul {sir}");
+
+
+
+        //2.Scrieti un program care sa verifice daca un sir de caractere citit de la tastatura este sau nu palindrom.
+
+        //Console.WriteLine("Introduceti textul:");
+        //string sir = Console.ReadLine();
+        // length = sir.Length;
+
+        //for (int i = 0; i < length / 2; i++)
+        //{
+        //if (sir[i] != sir[length - i - 1])
+        //{
+        //Console.WriteLine("Nu e palindrom");
+
+        //}
+        //}
+
+        //Console.WriteLine("E palindrom");
+
+
+
+        //4.Scrieti un program care va numara toate aparitiile unui caracter intr-un sir de caractere.Atat caracterul cautat cat si sirul de caractere vor fi citite de la tastatura
+
+        //Console.Write("Introduceti sirul de caractere: ");
+        //string text = Console.ReadLine();
+
+        //Console.WriteLine("Introduceti caracterul dorit:");
+        //char caracterdorit = Console.ReadKey().KeyChar;
+
+
+        //int count = 0;
+        //foreach (char c in text)
+        //{
+        //if (c == caracterdorit)
+        //{
+        //count++;
+        //}
+        //}
+        //Console.WriteLine($"Caracterul '{caracterdorit}' apare de {count} ori");
+
+
+
+        //6.Screiti un program care va numara vocalele dintr-un sir de caractere citit de la tastatura.
+
+        //Console.WriteLine("Introduceti textul:");
+        //string sir = Console.ReadLine();
+
+        //int numarvocale = NumaraVocale(sir);
+        //Console.WriteLine($"Numarul de vocale in sirul {sir} este de {numarvocale}.");
+    //}
+
+    //static int NumaraVocale(string input)
+   //{
+        //int count = 0;
+        //string vocale = "aeiouăâîAEIOUĂÂÎ";
+
+        //foreach (char c in input)
+        //{
+           //if (vocale.Contains(c))
+            //{
+                //count++;
+            //}
+        //}
+        //return count;
+    }
+}
